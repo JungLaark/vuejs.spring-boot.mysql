6727fc1c9a1e4f2db00ee55985fb426ef51d30e5
Chan-Sik Jung <chenone839@gmail.com>
2021-04-19 15:25:53 +0900

    test333

--- src/test/java/com/taskagile/AppApplicationTests.java
+++ src/test/java/com/taskagile/AppApplicationTests.java
@@ -1,15 +1,13 @@
 package com.taskagile;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class AppApplicationTests {
+public class AppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
