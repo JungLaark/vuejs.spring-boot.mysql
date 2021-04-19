eb31019506af0893ade352c8641d39363d01e37f
Chan-Sik Jung <chenone839@gmail.com>
2021-04-19 11:01:52 +0900

    test2

---
+++ front-end/src/router.js
@@ -0,0 +1,26 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes: []
+
+// })
+import Vue from 'vue'
+import Router from 'vue-router'
+import LoginPage from '@/views/LoginPage'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  }]
+})
