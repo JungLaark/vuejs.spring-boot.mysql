b247790f76500d5aa9f7f3a22e85bc0084df7477
Chan-Sik Jung <chenone839@gmail.com>
2021-04-20 10:07:54 +0900

    register

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -12,6 +12,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginPage from '@/views/LoginPage'
+import RegisterPage from '@/views/RegisterPage'
 
 Vue.use(Router)
 
@@ -22,5 +23,10 @@ export default new Router({
     path: '/login',
     name: 'LoginPage',
     component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage
   }]
 })
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -6,5 +6,14 @@ module.exports = {
         target: 'http://localhost:8080'
       }
     }
+  },
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      style: [
+        'bootstrap/dist/css/bootstrap.min.css'
+      ]
+    }
   }
 }
+// 웹팩의 새로운 진입점을 만들어서 모든 서드 파티 스타일을 하나의 css파일로 묶는다
